8638a78c57ac540da59844d61160bf77c485c0b5
Patrick Davey <patrick@vibrantplanet.net>
2025-03-23 18:34:57 +1300

    add initial non-refactored guessing game

---
+++ guess.py
@@ -0,0 +1,31 @@
+import random
+
+def get_valid_input():
+    while True:
+        user_input = input("Guess a number between 0 and 100: ")
+        try:
+            guess = int(user_input)
+            if 0 <= guess <= 100:
+                return guess
+            else:
+                print("Oops! Please enter a number between 0 and 100.")
+        except ValueError:
+            print("That's not a valid number! Please enter an integer between 0 and 100.")
+
+def play_game():
+    secret_number = random.randint(0, 100)
+    print("\nWelcome to the Higher/Lower Guessing Game!")
+    print("I'm thinking of a number between 0 and 100. Can you guess what it is?")
+
+    while True:
+        guess = get_valid_input()
+        if guess < secret_number:
+            print("Too low! Try a higher number.\n")
+        elif guess > secret_number:
+            print("Too high! Try a lower number.\n")
+        else:
+            print(f"Congratulations! You guessed it right. The number was {secret_number}.")
+            break
+
+if __name__ == "__main__":
+    play_game()
